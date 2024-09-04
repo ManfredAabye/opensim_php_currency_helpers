02fad1a3bd3170dd4809f11c2f52ab61ab3cc9c6
ManfredAabye <zainhofer@gmx.de>
2024-09-04 21:14:24 +0200

    04092024

--- newlandtool2.php
+++ newlandtool2.php
@@ -1,14 +1,10 @@
 <?php
-
 // Configurations
-define('ENV_HELPER_URL',  'http://127.0.0.1/currency');
-define('ENV_HELPER_PATH', __DIR__ . '/currency');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'database_name');
 define('DB_USER', 'db_user_name');
 define('DB_PASS', 'db_password');
 define('SECRET_KEY', '123456789');
-define('SYSURL', ENV_HELPER_URL);
 define('USE_CURRENCY_SERVER', true);
 define('UUID_ZERO', '00000000-0000-0000-0000-000000000000');
 
